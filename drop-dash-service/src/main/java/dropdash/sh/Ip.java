57cbebf55372dd7989208d3a71d8a11616eaa078
Hakan Dilek <hakandilek@gmail.com>
2014-02-06 19:17:49 +0200

    enhance ip and uptime scripts

--- drop-dash-service/src/main/java/dropdash/sh/Ip.java
+++ drop-dash-service/src/main/java/dropdash/sh/Ip.java
@@ -11,36 +11,49 @@ public class Ip implements Command {
 
 	Command ip, ifconFig, curl;
 
+	String output;
+	StringBuffer error;
+
 	public Ip() {
 		ip = new ShellCommand("/bin/ip -oneline link show | /usr/bin/awk '{print $2}' | /bin/sed \"s/://\"");
-		ifconFig = new ShellCommand("/sbin/ifconfig | /bin/grep -B1 \"inet addr\" | /usr/bin/awk '{ if ( $1 == \"inet\" ) { print $2 } else if ( $2 == \"Link\" ) { printf \"%s:\";$1 } }' | /usr/bin/awk -F: '{ print $1\";\"$3 }'");
+		ifconFig = new ShellCommand(
+				"/sbin/ifconfig | /bin/grep -B1 \"inet addr\" | /usr/bin/awk '{ if ( $1 == \"inet\" ) { print $2 } else if ( $2 == \"Link\" ) { printf \"%s:\";$1 } }' | /usr/bin/awk -F: '{ print $1\";\"$3 }'");
 		curl = new ShellCommand("curl http://ipecho.net/plain; echo");
 	}
 
 	@Override
 	public String execute() {
-		String result = ip.execute();
-		if(Strings.isNullOrEmpty(ip.getError())) {
-			List<String> split = lineSplit(result);
-			result = spaceJoin(split);
-			
-			String cmd = String.format("for interface in %s;do for family in inet inet6;do /bin/ip -oneline -family $family addr show $interface | /bin/grep -v fe80 | /usr/bin/awk '{print $2\";\"$4}';done;done", result);
+		error = new StringBuffer();
+		output = ip.execute();
+		if (Strings.isNullOrEmpty(ip.getError())) {
+			error.append(ip.getError());
+
+			List<String> split = lineSplit(output);
+			output = spaceJoin(split);
+
+			String cmd = String
+					.format("for interface in %s;do for family in inet inet6;do /bin/ip -oneline -family $family addr show $interface | /bin/grep -v fe80 | /usr/bin/awk '{print $2\";\"$4}';done;done",
+							output);
 			Command fam = new ShellCommand(cmd);
-			result = fam.execute();
+			output = fam.execute();
+			error.append(fam.getError());
 		} else {
-			result = ifconFig.execute();
+			output = ifconFig.execute();
+			error.append(ifconFig.getError());
 		}
 		String extIp = curl.execute();
+		error.append(curl.getError());
+
 		List<List<String>> out = Lists.newArrayList();
 		out.add(Lists.newArrayList("external ip", extIp));
-		
-		List<String> lines = lineSplit(result);
+
+		List<String> lines = lineSplit(output);
 		for (String line : lines) {
 			out.add(Lists.newArrayList(semicolSplit(line)));
 		}
-		
-		result = json(out);
-		return result;
+
+		output = json(out);
+		return output;
 	}
 
 	public int getResult() {
@@ -48,11 +61,11 @@ public class Ip implements Command {
 	}
 
 	public String getError() {
-		return null;
+		return error + "";
 	}
 
 	public String getOutput() {
-		return null;
+		return output;
 	}
 
 }
--- drop-dash-service/src/main/java/dropdash/sh/Uptime.java
+++ drop-dash-service/src/main/java/dropdash/sh/Uptime.java
@@ -13,30 +13,33 @@ public class Uptime extends FilteredCommand {
 	@Override
 	public String apply(String input) {
 		List<String> times = spaceSplit(input);
-		String timeString = times.get(0);
-		double totalSecs = Double.valueOf(timeString);
-		double totalMins = totalSecs / 60;
-		double totalHours = totalMins / 60;
+		if (times.size() > 0) {
+			String timeString = times.get(0);
+			double totalSecs = Double.valueOf(timeString);
+			double totalMins = totalSecs / 60;
+			double totalHours = totalMins / 60;
 
-		int days = (int) floor(totalHours / 24);
-		int hours = (int) floor(totalHours - (days * 24));
-		int mins = (int) floor(totalMins - (days * 60 * 24) - (hours * 60));
+			int days = (int) floor(totalHours / 24);
+			int hours = (int) floor(totalHours - (days * 24));
+			int mins = (int) floor(totalMins - (days * 60 * 24) - (hours * 60));
 
-		StringBuffer sb = new StringBuffer();
-		if (days != 0) {
-			sb.append(days).append(" days ");
+			StringBuffer sb = new StringBuffer();
+			if (days != 0) {
+				sb.append(days).append(" days ");
+			}
+
+			if (hours != 0) {
+				sb.append(hours).append(" hours ");
+			}
+
+			if (mins != 0) {
+				sb.append(mins).append(" minutes");
+			}
+
+			String value = sb.toString();
+			return json(value);
 		}
-
-		if (hours != 0) {
-			sb.append(hours).append(" hours ");
-		}
-
-		if (mins != 0) {
-			sb.append(mins).append(" minutes");
-		}
-
-		String value = sb.toString();
-		return json(value);
+		return null;
 	}
 
 }
